cfe1037504aefde48867ef152103d9a8be3fad22
bohdan062282 <bogdan.suprun2012@gmail.com>
2020-03-05 10:12:46 +0300

    oxy2

--- FL1/MainWindow.xaml.cs
+++ FL1/MainWindow.xaml.cs
@@ -22,28 +22,35 @@ namespace FL1
     {
         public MainViewModel()
         {
-            this.Title = "Example 2";
-            this.Points1 = new List<DataPoint>
-                              {
-                                  new DataPoint(0, 4),
-                                  new DataPoint(10, 13),
-                                  new DataPoint(20, 15),
-                                  new DataPoint(30, 16),
-                                  new DataPoint(40, 12),
-                                  new DataPoint(50, 12)
-                              };
-            this.Points2 = new List<DataPoint>
-                              {
-                                  new DataPoint(0, 1),
-                                  new DataPoint(10, 13),
-                                  new DataPoint(40, 5)
-                              };
+            this.Title = "Graph";
+            setDefaultPoints();
+        }
+        public void setPointsX(float aLX, float aRX, float bLX, float bRX, float cLX, float cRX)
+        {
+            Points1 = new List<DataPoint> { new DataPoint(aLX, 1), new DataPoint(aRX, 1) };
+            Points2 = new List<DataPoint> { new DataPoint(bLX, 2), new DataPoint(bRX, 2) };
+            Points3 = new List<DataPoint> { new DataPoint(cLX, 3), new DataPoint(cRX, 3) };
+        }
+        public void setDefaultPoints()
+        {
+            setPointsX(0, 0, 0, 0, 0, 0);
+        }
+        public void setGraphs(TextBox fstLeft, TextBox fstRight, TextBox sndLeft, TextBox sndRight, TextBlock res)
+        {
+            string[] tmp = res.Text.Split(' ');
+            setPointsX(float.Parse(fstLeft.Text), float.Parse(fstRight.Text), float.Parse(sndLeft.Text), float.Parse(sndRight.Text), float.Parse(tmp[0]), float.Parse(tmp[1]));
+        }
+        public void setGraphs(TextBox fstLeft, TextBox fstRight, TextBlock res)
+        {
+            string[] tmp = res.Text.Split(' ');
+            setPointsX(float.Parse(fstLeft.Text), float.Parse(fstRight.Text), 0, 0, float.Parse(tmp[0]), float.Parse(tmp[1]));
         }
 
         public string Title { get; private set; }
 
         public IList<DataPoint> Points1 { get; private set; }
         public IList<DataPoint> Points2 { get; private set; }
+        public IList<DataPoint> Points3 { get; private set; }
     }
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
@@ -55,6 +62,7 @@ namespace FL1
         public MainWindow()
         {
             InitializeComponent();
+            viewModel.setPointsX(0, 1, 3, 4, 5, 6);
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
@@ -138,27 +146,29 @@ namespace FL1
         }
 
         private void Calculate_Click(object sender, RoutedEventArgs e)
-        {
+        {           
+            viewModel.setPointsX(200, 56, 3, 4, 58, 7);
+            model.InvalidatePlot(true);
             try
             {
                 switch (this.operationsBox.SelectedIndex)
                 {
-                    case 0: checkInterv(aLeft, aRight); checkInterv(bLeft, bRight); this.result.Text = addition(aLeft, aRight, bLeft, bRight); break;
-                    case 1: checkInterv(aLeft, aRight); checkInterv(bLeft, bRight); this.result.Text = subtraction(aLeft, aRight, bLeft, bRight); break;
-                    case 2: checkInterv(aLeft, aRight); checkInterv(bLeft, bRight); this.result.Text = multiplication(aLeft, aRight, bLeft, bRight); break;
-                    case 3: checkInterv(aLeft, aRight); checkInterv(bLeft, bRight); this.result.Text = division(aLeft, aRight, bLeft, bRight); break;
-                    case 4: checkInterv(aLeft, aRight); this.result.Text = display(aLeft, aRight); break;
-                    case 5: checkInterv(bLeft, bRight); this.result.Text = inversion(bLeft, bRight); break;
-                    case 6: checkInterv(aLeft, aRight); this.result.Text = addition(aLeft, aRight, additional, additional); break;
-                    case 7: checkInterv(bLeft, bRight); this.result.Text = subtraction(bLeft, bRight, additional, additional); break;
-                    case 8: checkInterv(aLeft, aRight); this.result.Text = multiplication(aLeft, aRight, additional, additional); break;
-                    case 9: checkInterv(bLeft, bRight); this.result.Text = division(bLeft, bRight, additional, additional); break;
+                    case 0: checkInterv(aLeft, aRight); checkInterv(bLeft, bRight); this.result.Text = addition(aLeft, aRight, bLeft, bRight); viewModel.setGraphs(aLeft, aRight, bLeft, bRight, result); viewModel.setPointsX(0, 1, 3, 4, 5, 6); break;
+                    case 1: checkInterv(aLeft, aRight); checkInterv(bLeft, bRight); this.result.Text = subtraction(aLeft, aRight, bLeft, bRight); viewModel.setGraphs(aLeft, aRight, bLeft, bRight, result); break;
+                    case 2: checkInterv(aLeft, aRight); checkInterv(bLeft, bRight); this.result.Text = multiplication(aLeft, aRight, bLeft, bRight); viewModel.setGraphs(aLeft, aRight, bLeft, bRight, result); break;
+                    case 3: checkInterv(aLeft, aRight); checkInterv(bLeft, bRight); this.result.Text = division(aLeft, aRight, bLeft, bRight); viewModel.setGraphs(aLeft, aRight, bLeft, bRight, result); break;
+                    case 4: checkInterv(aLeft, aRight); this.result.Text = display(aLeft, aRight); viewModel.setGraphs(aLeft, aRight, result); break;
+                    case 5: checkInterv(bLeft, bRight); this.result.Text = inversion(bLeft, bRight); viewModel.setGraphs(aLeft, aRight, result); break;
+                    case 6: checkInterv(aLeft, aRight); this.result.Text = addition(aLeft, aRight, additional, additional); viewModel.setGraphs(aLeft, aRight, additional, additional, result); break;
+                    case 7: checkInterv(bLeft, bRight); this.result.Text = subtraction(bLeft, bRight, additional, additional); viewModel.setGraphs(bLeft, bRight, additional, additional, result); break;
+                    case 8: checkInterv(aLeft, aRight); this.result.Text = multiplication(aLeft, aRight, additional, additional); viewModel.setGraphs(aLeft, aRight, additional, additional, result); break;
+                    case 9: checkInterv(bLeft, bRight); this.result.Text = division(bLeft, bRight, additional, additional); viewModel.setGraphs(bLeft, bRight, additional, additional, result); break;
                     //case 10: multMultiplication(); break;
                     default: throw new Exception();
                 }
                 string[] tmp = result.Text.Split(' ');
                 if (float.Parse(tmp[0]) >= float.Parse(tmp[1])) throw new InvalidResultException();
-        }
+            }
             catch (InvalidResultException)
             {
                 this.result.Text = null;
@@ -167,7 +177,6 @@ namespace FL1
             catch
             {
                 MessageBox.Show("Невірні параметри!", "Помилка!");
-                
             }
 
         }
@@ -196,7 +205,7 @@ namespace FL1
             }         
             catch
             {
-                MessageBox.Show("Невірні параметри!", "Помилка!");             
+                MessageBox.Show("Невірні параметри!", "Помилка!");
             }
         }
 
